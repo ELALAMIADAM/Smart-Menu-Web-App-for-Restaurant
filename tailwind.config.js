67ae8663fdfe3de7d1f90c57a6ea731bcf2d0b77
ADAM EL ALAMI <154709882+ELALAMIADAM@users.noreply.github.com>
2024-10-18 00:30:39 +0200

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-    mode: "jit", // faster dev
-    content: ["./src/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-      extend: {
-        margin: {
-          marrow: "85rem",
-        },
+  mode: "jit", // Just-in-Time mode for faster builds in development
+  content: [
+    "./src/index.html", 
+    "./src/**/*.{js,jsx,ts,tsx}" // Ensures Tailwind scans all relevant files
+  ],
+  theme: {
+    extend: {
+      margin: {
+        marrow: "85rem", // Custom margin value
       },
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+};
